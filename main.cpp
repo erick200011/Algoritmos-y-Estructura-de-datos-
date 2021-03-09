fefff71be0550b5a3a2628f4edf8aea5d64f036f
Erick Palomo <epalomo154@gmail.com>
2021-03-09 14:56:10 +0000

    En este programa podemos observar la aplicación del algoritmo Quicksort

--- main.cpp
+++ main.cpp
@@ -1,113 +1,52 @@
 #include <iostream>
-using namespace std;
 #include <fstream>
-void carrera(string nombrearchivo);
-void mostrar(string nombrearchivo);
-void Menu();
-int main() 
-{
-  string nombrearchivo;
-  char Tipo;
-  int con=1;
-  while(con!=0)
-  {
-    Menu();
-    cout<<" \n\nIngrese una opcion: ";
-    cin>>Tipo;
-    switch (Tipo) 
-	  {
-      case 'a':
-      cin.ignore();
-        cout<<"Ingrese el nombre del archivo:\n";
-        getline(cin, nombrearchivo);
-        carrera(nombrearchivo);
-        con=1;
-       break;          
-	    case 'b':
-         mostrar(nombrearchivo);
-         con=1;
-       break;          
-	    case 'c':       
-       con=0;
-       cout<<"\n-------MUCHAS GRACIAS ------\n";
-       break;
-	    default:cout<<"\nERROR, Al introducir la opcion\n";
-       con=1;
-       break;
-     }
-    
-  }
-  return 0;
-}
-void carrera(string nombrearchivo)
-{
-  string nombre;
-  string apellido;
-  string carrera;
-  string edad;
-  char r;
-  ofstream archivoprueba;
-  archivoprueba.open(nombrearchivo,ios::out);
-  do
-  {
-    cout<<"Ingrese el nombre:\n";
-    getline(cin,nombre);
-    cout<<"Ingrese el apellido:\n";
-    getline(cin,apellido);
-    cout<<"Ingrese la edad:\n";
-    cin>>edad;
-    cout<<"Ingrese su carrera:\n";
-    cin >>carrera;
-    archivoprueba<<nombre<<" "<<apellido<<" "<<edad<<" "<<carrera<<endl;
-    cout<<"Desea ingresar otro contacto s/n:\n";
-    cin>>r;
-    cin.ignore();
-  }
-  while (r=='s');
-  archivoprueba.close();
+#include <stdlib.h> 
+using namespace std;
+
+
+
+void quickSort(int *A, int izq, int der) {  
+ int piv = A[izq];
+ int i = izq;
+ int j = der;
+ int aux;
+
+ while (i < j) {
+    while (A[i] <= piv && i < j) i++;
+      while (A[j] > piv) j--;
+        if (i < j) {
+          aux  = A[i];
+          A[i] = A[j];
+          A[j] = aux;
+          }
+ }
+
+ A[izq] = A[j];
+ A[j] = piv;
+ if (izq<j - 1)
+  quickSort(A, izq, j - 1);
+ if (j + 1 <der)
+  quickSort(A, j + 1, der);
+
 }
 
-void mostrar (string nombrearchivo)
-{
-  string nombre;
-  string apellido;
-  string edad;
-  string carrera;
-  char r;
-  ofstream archivoprueba;
-  
-   ifstream archivolectura;
-   string texto;
-   archivolectura.open(nombrearchivo,ios::in);
-   if (archivolectura.fail())
-   {
-     cout<<"NO SE ENCONTRÓ NINGÚN ARCHIVO!\n";
-   }
-   else
-   {
-      while (!archivolectura.eof())
-    {
-      archivolectura>>nombre>>apellido>>edad>>carrera;
-      if(!archivolectura.eof())
-      {
-          cout<<"Nombre: "<<nombre<<endl;
+int main() {
+  ofstream archivo;
+  archivo.open("\nOrdenamiento QuickSort\n",ios::app);
+ int A[10];
+ cout << " El array de valores es: " << endl;
+ for (int i = 0; i < 10; i++) {
+  A[i] = rand();
+  cout << A[i] << " ";
+ }
+ cout << endl << " Y el array de valores ordenados es: " << endl;
+ quickSort(A, 0, 9);
 
-          cout<<"Apellido: "<<apellido<<endl;
-
-          cout<<"Edad: "<<edad<<endl;
-
-          cout<<"Carrera: "<<carrera<<endl;
-      } 
-    }
-   }
-    archivolectura.close();
-}
-void Menu(void)
-{
-  cout<<"\t\t╔══════════════════════════════════════════╗\n";
-  cout<<"\t\t║                █ MENÚ █                  ║\n";
-	cout<<"\t\t║  a) INGRESAR DATOS                       ║\n";
-	cout<<"\t\t║  b) MOSTRAR DATOS                        ║\n";
-	cout<<"\t\t║  c) Salir                                ║\n";
-  cout<<"\t\t╚══════════════════════════════════════════╝\n";
+ for (int i = 0; i < 10; i++) {
+  cout << A[i] << " ";
+  archivo <<A[i]<<" ";
+ }
+ system("pause > nul");
+ archivo.close();
+ return 0;
 }
