ad24926ed7787d9c9951b9397d0e1e04112ec638
Erick Palomo <epalomo154@gmail.com>
2021-03-11 16:14:32 +0000

    Este programa lo que hace es ver el ordenamiento de arreglos por "heapsort"

--- main.cpp
+++ main.cpp
@@ -1,113 +1,58 @@
 #include <iostream>
-using namespace std;
 #include <fstream>
-void carrera(string nombrearchivo);
-void mostrar(string nombrearchivo);
-void Menu();
-int main() 
-{
-  string nombrearchivo;
-  char Tipo;
-  int con=1;
-  while(con!=0)
-  {
-    Menu();
-    cout<<" \n\nIngrese una opcion: ";
-    cin>>Tipo;
-    switch (Tipo) 
-	  {
-      case 'a':
-      cin.ignore();
-        cout<<"Ingrese el nombre del archivo:\n";
-        getline(cin, nombrearchivo);
-        carrera(nombrearchivo);
-        con=1;
-       break;          
-	    case 'b':
-         mostrar(nombrearchivo);
-         con=1;
-       break;          
-	    case 'c':       
-       con=0;
-       cout<<"\n-------MUCHAS GRACIAS ------\n";
-       break;
-	    default:cout<<"\nERROR, Al introducir la opcion\n";
-       con=1;
-       break;
-     }
-    
-  }
-  return 0;
-}
-void carrera(string nombrearchivo)
-{
-  string nombre;
-  string apellido;
-  string carrera;
-  string edad;
-  char r;
-  ofstream archivoprueba;
-  archivoprueba.open(nombrearchivo,ios::out);
-  do
-  {
-    cout<<"Ingrese el nombre:\n";
-    getline(cin,nombre);
-    cout<<"Ingrese el apellido:\n";
-    getline(cin,apellido);
-    cout<<"Ingrese la edad:\n";
-    cin>>edad;
-    cout<<"Ingrese su carrera:\n";
-    cin >>carrera;
-    archivoprueba<<nombre<<" "<<apellido<<" "<<edad<<" "<<carrera<<endl;
-    cout<<"Desea ingresar otro contacto s/n:\n";
-    cin>>r;
-    cin.ignore();
-  }
-  while (r=='s');
-  archivoprueba.close();
-}
+using namespace std;
 
-void mostrar (string nombrearchivo)
+void HeapSort(int b[], int n);
+
+int main()
 {
-  string nombre;
-  string apellido;
-  string edad;
-  string carrera;
-  char r;
-  ofstream archivoprueba;
-  
-   ifstream archivolectura;
-   string texto;
-   archivolectura.open(nombrearchivo,ios::in);
-   if (archivolectura.fail())
-   {
-     cout<<"NO SE ENCONTRÓ NINGÚN ARCHIVO!\n";
-   }
-   else
-   {
-      while (!archivolectura.eof())
+    ofstream archivo;
+    archivo.open("HEAPSORT.txt",ios::app);
+    int A[100],n;
+    cout<<"Ingresa la cantidad de elementos del arreglo: ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
     {
-      archivolectura>>nombre>>apellido>>edad>>carrera;
-      if(!archivolectura.eof())
-      {
-          cout<<"Nombre: "<<nombre<<endl;
+       cin >> A[i];
+      HeapSort(A,n);
+      archivo<<endl<<"Arreglo original:"<<endl<<A[i]<<endl;
 
-          cout<<"Apellido: "<<apellido<<endl;
-
-          cout<<"Edad: "<<edad<<endl;
-
-          cout<<"Carrera: "<<carrera<<endl;
-      } 
     }
-   }
-    archivolectura.close();
+   
+    cout<<"El orden es:";
+    for(int i=1;i<=n;i++)
+    {
+      cout<<A[i];
+      archivo<<endl<<"Arreglo ordenado es:"<<endl<<A[i];
+
+    }
+    archivo.close();
+    
+
+    return 0;
 }
-void Menu(void)
+
+void HeapSort(int b[], int n)
 {
-  cout<<"\t\t╔══════════════════════════════════════════╗\n";
-  cout<<"\t\t║                █ MENÚ █                  ║\n";
-	cout<<"\t\t║  a) INGRESAR DATOS                       ║\n";
-	cout<<"\t\t║  b) MOSTRAR DATOS                        ║\n";
-	cout<<"\t\t║  c) Salir                                ║\n";
-  cout<<"\t\t╚══════════════════════════════════════════╝\n";
+    int valor,temp,a;
+
+    for(int i=n; i>0; i--)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            valor=b[j];
+            a=j/2;
+            while(a>0 && b[a]<valor)
+            {
+                b[j]=b[a];
+                j=a;
+                a=a/2;
+            }
+            b[j]=valor;
+        }
+        temp=b[1];
+        b[1]=b[i];
+        b[i]=temp;
+    }
+
 }
